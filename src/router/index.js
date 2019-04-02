b57972667cb7bfaf7a4633235fd153e4470dc531
Muhammad Edward Chakim <muhedwardc@gmail.com>
2019-04-02 22:32:14 +0700

    change routing mode

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import routes from './routes'
 Vue.use(Router)
 
 const router = new Router({
+  mode: 'history',
   routes: routes,
   scrollBehavior (to, from, savedPosition) {
     if (savedPosition) {
