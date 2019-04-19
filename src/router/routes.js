b0559da7206eafd3062709a314490c1ecf9443a7
Muhammad Edward Chakim <muhedwardc@gmail.com>
2019-04-19 16:35:17 +0700

    Adding router meta data

--- src/router/routes.js
+++ src/router/routes.js
@@ -6,63 +6,110 @@ export default [
     {
         path: '/login',
         name: 'Login',
+        meta: { publicRoute: true },
         component: () => import('@/views/login.vue')
     },
     {
         path: '/akademik',
         name: 'Akademik Home',
+        meta: {
+            requiresAuth: true,
+            akademikRoute: true,
+        },
         component: () => import('@/views/akademik/home.vue')
     },
     {
         path: '/daftar-pengguna',
         name: 'Daftar Pengguna',
+        meta: {
+            requiresAuth: true,
+            akademikRoute: true,
+        },
         component: () => import('@/views/akademik/list/user.vue')
     },
     {
         path: '/dosen',
         name: 'Dosen Home',
+        meta: {
+            requiresAuth: true,
+            dosenRoute: true,
+        },
         component: () => import('@/views/dosen/home.vue')
     },
     {
         path: '/dosen/profil',
         name: 'Dosen Profil',
+        meta: {
+            requiresAuth: true,
+            dosenRoute: true,
+        },
         component: () => import('@/views/dosen/profil.vue')
     },
     {
         path: '/dosen/ujian',
         name: 'Dosen Ujian',
+        meta: {
+            requiresAuth: true,
+            dosenRoute: true,
+        },
         component: () => import('@/views/dosen/ujian.vue')
     },
     {
         path: '/ujian',
         name: 'Ujian Show',
+        meta: {
+            requiresAuth: true,
+            akademikRoute: true
+        },
         component: () => import('@/views/akademik/list/exam.vue')
     },
     {
         path: '/ujian/tambah',
         name: 'Akademik Add Ujian',
+        meta: {
+            requiresAuth: true,
+            akademikRoute: true,
+        },
         component: () => import('@/views/akademik/add/exam.vue')
     },
     {
         path: '/ujian/:id',
         name: 'Ujian Detail',
+        meta: {
+            requiresAuth: true,
+            dosenRoute: true,
+            akademikRoute: true
+        },
         component: () => import('@/views/ujian/show.vue')
     },
     {
         path: '/ujian/:id/mulai',
         name: 'Ujian Mulai',
-        meta: { exam: true },
+        meta: { 
+            exam: true,
+            requiresAuth: true,
+            dosenRoute: true
+        },
         component: () => import('@/views/ujian/start.vue')
     },
     {
         path: '/ujian/:id/tunggu',
         name: 'Ruang Ujian',
-        meta: { exam: true },
+        meta: { 
+            exam: true,
+            requiresAuth: true,
+            dosenRoute: true
+        },
         component: () => import('@/views/ujian/waiting.vue')
     },
     {
         path: '/playground',
         name: 'Playground',
+        meta: {
+            requiresAuth: true,
+            dosenRoute: true,
+            akademikRoute: true
+        },
         component: () => import('@/views/playground/index.vue')
     }
 ]
