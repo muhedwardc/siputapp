e07ff36e883008eaf468b9e7a6d5260960cda3fc
muhashil <muh.ashil@yahoo.com>
2019-05-03 05:14:49 +0700

    add StudentOutcome model and change Grade model

--- backend/grades/models.py
+++ backend/grades/models.py
@@ -3,15 +3,18 @@ from django.db import models
 from backend.essays.models import Student
 from backend.exams.models import Penguji
 
+class StudentOutcome(models.Model):
+    judul = models.CharField(max_length=5)
+    deksripsi = models.TextField()
+
+    def __str__(self):
+        return self.judul
+
 class Grade(models.Model):
     mahasiswa = models.ForeignKey(Student, related_name='grades', on_delete=models.CASCADE)
     penguji = models.ForeignKey(Penguji, related_name='grades', on_delete=models.DO_NOTHING)
-    so1 = models.IntegerField()
-    so2 = models.IntegerField()
-    so3 = models.IntegerField()
-    so4 = models.IntegerField()
-    so5 = models.IntegerField()
-    so6 = models.IntegerField()
+    so = models.ForeignKey(StudentOutcome, on_delete=models.DO_NOTHING, blank=True, null=True)
+    nilai = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True, default=0)
 
     def __str__(self):
         return self.mahasiswa.nama
