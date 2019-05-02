93f850d4f86db6e0f7ffb50802b97ed9f8867d35
Muhammad Edward Chakim <muhedwardc@gmail.com>
2019-05-02 22:32:22 +0700

    Redesign 404 error page

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,7 +1,8 @@
 <template>
     <v-layout class="container-404" align-center justify-center column>
-        Maaf Halaman yang anda tuju tidak ditemukan.
-        <v-btn class="primary" @click="$router.push('/')">Kembali ke beranda</v-btn>
+        <img src="@/assets/404-error.svg" class="img-404">
+        Maaf, halaman yang Anda tuju tidak ditemukan.
+        <v-btn class="primary mt-2" @click="$router.push('/')">Kembali ke beranda</v-btn>
     </v-layout>
 </template>
 
@@ -9,6 +10,12 @@
     .container-404 {
         width: 100%;
         height: 100%;
+    }
+
+    .img-404 {
+        max-width: 200px;
+        margin-bottom: 16px;
+        display: block;
     }
 </style>
 
