a668220fcb01ff203dc0aa142e462ad794f953f5
Muhammad Edward Chakim <muhedwardc@gmail.com>
2019-05-11 16:41:27 +0700

    adjusting plugins

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
+import colors from 'vuetify/es5/util/colors'
 import 'vuetify/src/stylus/app.styl'
 
 Vue.use(Vuetify, {
   iconfont: 'md',
+  theme: {
+    primary: colors.indigo
+  }
 })
