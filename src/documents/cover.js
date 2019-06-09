ee87b5228f02179744b887c35901eef7ea4e070e
Muhammad Edward Chakim <muhedwardc@gmail.com>
2019-06-09 16:28:03 +0700

    adding document cover

---
+++ src/documents/cover.js
@@ -0,0 +1,54 @@
+import UGMLogo from './partials/ugm-logo'
+
+function generateDoc(data) {
+    const { tanggal, hari, judul, ruang, waktu, dosen, sekretaris, mahasiswa } = data
+    const mahasiswaList = mahasiswa.map(mhs => { return [{text: mhs.nama + '\n' + mhs.nim, alignment: 'center', fontSize: 20, margin: [0, 0, 0, 10]}] })
+    let doc = [
+        {
+            table: {
+                widths: [ '*' ],
+                heights: [ 720 ],
+                body: [
+                    [
+                        {
+                            table: {
+                                widths: [ '*' ],
+                                body: [
+                                    [ {text: 'BERKAS PENDADARAN', alignment: 'center', fontSize: 26, margin: [0, 50, 0, 50], bold: true, decoration: 'underline'} ],
+                                    [
+                                        {
+                                            table: {
+                                                widths: ['auto', 'auto', '*'],
+                                                body: [
+                                                    [ 'HARI', ':', hari ],
+                                                    [ 'TANGGAL', ':', tanggal ],
+                                                    [ 'WAKTU', ':', waktu ],
+                                                    [ 'RUANG', ':', ruang ],
+                                                ],
+                                            },
+                                            fontSize: 20,
+                                            layout: 'noBorders',
+                                            margin: [40, 0, 0, 0]
+                                        }
+                                    ],
+                                    [ {text: 'Mahasiswa: ', alignment: 'center', fontSize: 20, margin: [0, 50, 0, 15]} ],
+                                    ...mahasiswaList,
+                                ],
+                            },
+                            layout: 'noBorders',
+                            margin: [0, 0, 0, 50]
+                        }
+                    ]
+                ]
+            },
+            margin: [40, 40, 40, 40],
+        }
+    ]
+
+    return doc
+}
+
+export default function (data) {
+    let doc = generateDoc(data)
+    return doc
+}
