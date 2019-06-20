b6f24d27fc048026bb23aa076c35979895ed91fe
Muhammad Edward Chakim <muhedwardc@gmail.com>
2019-06-20 17:03:47 +0700

    add get recap, assign new dosen, edit exam & delete exam

--- src/apis/exam.js
+++ src/apis/exam.js
@@ -1,5 +1,6 @@
 import axios from './instance'
 import store from '../store'
+import { async } from 'q';
 
 export default {
     getAllExams: async () => await axios.get('/exams/'),
@@ -11,11 +12,17 @@ export default {
     getExamComments: async (id) => await axios.get(`/me/exams/${id}/comments/`),
     getExamGrades: async (id) => await axios.get(`/me/exams/${id}/grades/`),
     getExamRevision: async (id) => await axios.get(`/me/exams/${id}/revision/`),
-    getExamRecap: async (id) => await axios.get(`/exams/${id}/recap/`),
+    getExamRecap: async (id) => await axios.get(`/me/exams/${id}/recap/`),
 
     createNewExam: async (payload) => await axios.post('/exams/', payload),
     addThesis: async (name, formData) => await axios.post(`/exams/upload-skripsi/${name}`, formData),
     submitComments: async (id, payload) => await axios.post(`/me/exams/${id}/comments/`, payload),
     submitGrades: async (id, payload) => await axios.post(`/me/exams/${id}/grades/`, payload),
     submitRevision: async (id, payload) => await axios.post(`/me/exams/${id}/revision/`, payload),
+    assignNewDosen: async (id, payload) => await axios.post(`/exams/${id}/penguji/`, payload),
+
+    editExamItem: async(id, payload) => await axios.put(`/exams/${id}/edit_exam/`, payload),
+    editEssayItem: async(id, payload) => await axios.put(`/exams/${id}/edit_essay/`, payload),
+
+    deleteExam: async (id) => await axios.delete(`/exams/${id}/`)
 }
