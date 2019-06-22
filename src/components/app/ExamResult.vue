c8aa7b8d5670ca14e4338d0120015979d23488b3
Muhammad Edward Chakim <muhedwardc@gmail.com>
2019-06-22 21:00:12 +0700

    add exam result button component

---
+++ src/components/app/ExamResult.vue
@@ -0,0 +1,93 @@
+<template>
+    <v-layout align-center>
+        <v-btn :disabled="fetchingData || generatingDocument" @click="getDocument" class="ma-0 mr-2" color="primary">
+            <v-icon left>file_download</v-icon>
+            Unduh
+        </v-btn>
+        <v-btn :disabled="fetchingData || generatingDocument" @click="getDocument('print')" class="ma-0" color="warning">
+            <v-icon left>print</v-icon>
+            Cetak
+        </v-btn>
+        <span class="black--text ml-2" v-text="message"></span>
+    </v-layout>
+</template>
+
+<script>
+import pdfMake from 'pdfmake/build/pdfmake.js'
+import pdfFonts from 'pdfmake/build/vfs_fonts.js'
+import documentGenerator from '@/documents/index.js'
+
+export default {
+    props: {
+        examId: {
+            type: Number | String,
+            required: true
+        }
+    },
+
+    data() {
+        return {
+            recap: null,
+            documentObj: null,
+            documentDefinition: null,
+            pdf: null,
+            fetchingData: false,
+            generatingDocument: false
+        }
+    },
+
+    computed: {
+        message() {
+            let message = this.fetchingData ? 'Mengambil data ...' : this.generatingDocument ? 'Memproses berkas ...' : null
+            return message
+        }
+    },
+
+    methods: {
+        async getDocument(type = 'download') {
+            this.fetchingData = true
+            try {
+                const response = await this.$thessa.getExamDocumentData(this.examId)
+                this.recap = response.data
+                this.fetchingData = false
+                this.generateDocument(type)
+            } catch (error) {
+                this.$store.dispatch('showSnackbar', error.message ? error.message : error)
+                this.fetchingData = false
+            }
+        },
+
+        generateDocument(type = 'download') {
+            try {
+
+                this.generatingDocument = true
+                this.documentObj = documentGenerator(this.recap)
+                if (this.documentObj) {
+                    if (pdfMake.vfs == undefined) {
+                        pdfMake.vfs = pdfFonts.pdfMake.vfs
+                    }
+        
+                    this.docDefinition = { 
+                        pageOrientation: 'portrait',
+                        content: this.documentObj,
+                        defaultStyle: {
+                            fontSize: 10,
+                            lineHeight: 1
+                        },
+                        pageSize: 'A4',
+                        pageMargins: [ 30, 20, 30, 20 ]
+                    }
+        
+                    let pdf = pdfMake.createPdf(this.docDefinition)
+                    if (type == 'print') pdf.print()
+                    else pdf.download()
+                }
+                this.generatingDocument = false
+            } catch (error) {
+                this.generatingDocument = false
+            }
+        }
+    }
+}
+</script>
+
