b90b769944b00e9b6b7bf0d9c8f9edbad0dd644a
Muhammad Edward Chakim <muhedwardc@gmail.com>
2019-06-28 15:40:36 +0700

    add managers api endpoint

--- src/apis/user.js
+++ src/apis/user.js
@@ -5,8 +5,10 @@ export default {
     getAllDosen: async (qs) => await axios.get(`/users/dosen/${qs ? '?' + qs : ''}`),
     getAllAkademik: async (qs) => await axios.get(`/users/akademik/${qs ? '?' + qs : ''}`),
     getMyProfile: async () => await axios.get('/me/profile/'),
+    getManagers: async () => await axios.get('/users/pengelola/'),
 
     createNewUser: async (role, payload) => await axios.post(`/users/${role.toLowerCase()}/`, payload),
+    postManager: async (payload) => await axios.post('/users/pengelola/', payload),
 
     updateUser: async (id, payload) => await axios.put(`/users/${id}/`, payload),
     updateMyProfile: async (payload) => await axios.put('/me/profile/', payload),
