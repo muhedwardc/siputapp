03e968b86f347b2c9b46a0ad7433892dbf0ceb79
Muhammad Edward Chakim <muhedwardc@gmail.com>
2019-07-15 22:03:39 +0700

    adjusting webpack

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,14 @@
 var path = require('path')
 var BrotliGzipPlugin = require('brotli-gzip-webpack-plugin')
 var PrerenderSPAPlugin = require('prerender-spa-plugin')
-// const IS_PRODUCTION = process.env.NODE_ENV === 'production'
+const IS_PRODUCTION = process.env.NODE_ENV === 'production'
 
 module.exports = {
     outputDir: 'dist',
     assetsDir: 'static',
-    // baseUrl: IS_PRODUCTION
-    // ? 'http://cdn123.com'
-    // : '/',
+    publicPath: IS_PRODUCTION
+      ? 'http://localhost:8000'
+      : '/',
     // For Production, replace set baseUrl to CDN
     // And set the CDN origin to `yourdomain.com/static`
     // Whitenoise will serve once to CDN which will then cache
